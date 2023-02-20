c4d525dbdba9362f5b2681fb3fc5eb07332e635f
toxuh <zakharov.toxa@gmail.com>
2023-02-20 19:25:52 +0100

    Chakra UI learning:
    - Theming
    - Navbar start

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import theme from './theme'
 
 import App from './containers/App/App'
 
-import './index.sass'
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
