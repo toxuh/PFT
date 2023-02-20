be4a7d3f3d8675b21783c94ed73aa8dfcdcd4acc
toxuh <zakharov.toxa@gmail.com>
2023-02-20 18:24:34 +0100

    Initialization.
    - prettier
    - eslint
    - Chakra UI as UI library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import App from './containers/App/App'
+
+import './index.sass'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
